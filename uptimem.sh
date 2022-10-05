0c57687745b9d9d54f99da79152aee7597c22255
Wasiak Florke Daniel <wasiak.daniel@gmail.com>
2022-10-05 13:09:16 +0200

    Sending e-mail when connection is restored

--- uptimem.sh
+++ uptimem.sh
@@ -15,20 +15,25 @@ BODY_FAIL="You will be informed when it is back up via separate e-mail."
 BODY_SUCC="Remote machine answered the ping."
 
 notification() {
-	
-
+    status="$2"
     curtime="$(date --iso-8601=seconds)"
     maildir="/tmp/mail/$curtime/"
     mkdir -p $maildir
     mail="$MAILDIR/notification_message.txt"
-    
-    echo $SUBJECT_FAIL > $mail
 
-    echo $BODY_FAIL >> $mail
+    if $status -eq "1"
+    then
+        echo "$SUBJECT_FAIL" > $mail
+        echo "$BODY_FAIL" >> $mail
+    else    
+        echo "$SUBJECT_SUCC" > $mail
+        echo "$BODY_SUCC" >> $mail
+    fi
+    
     echo "---" >> $mail
     echo "$curtime" >> $mail
     
-    sendmail $SUBSCRIBERS < $mail
+    sendmail "$SUBSCRIBERS" < $mail
 }
 
 test() {
@@ -41,28 +46,28 @@ test() {
         echo "Basic test failed."
         test "$1" 4 && return 1
     fi
-    
+
     return 1
 }
 
 main() {
     for host in $HOSTS; do
         lockfile="$CACHE_DIR/$host"
-    
+
         if test "$host" 1
         then
             echo "Ping succeess!"
-            rm -f "$lockfile"
+            rm -f "$lockfile" && notification $host "1"
         else
             ls $CACHE_DIR > /dev/null || mkdir -p $CACHE_DIR
-            
+
             if ls "$lockfile" > /dev/null
             then
                 echo "Uptime check with $host failed once again."
             else
                 echo "[!] Uptime check with $host failed."
                 touch "$lockfile"
-                notification $host
+                notification $host "0"
             fi
         fi
     done
