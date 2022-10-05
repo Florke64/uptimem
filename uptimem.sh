88970d61458741c3345c8d1c6cd067c667ecb110
Wasiak Florke Daniel <wasiak.daniel@gmail.com>
2022-10-05 12:45:22 +0200

    Replaced e-mail subject and body with variables

--- uptimem.sh
+++ uptimem.sh
@@ -8,15 +8,21 @@ HOSTS="$@"
 SUBSCRIBERS=$(cat "subscribers.txt")
 CACHE_DIR="$HOME/.cache/uptimem"
 
+# E-Mail Content
+SUBJECT_FAIL="Subject: $ID: Uptime check failed with $1"
+SUBJECT_SUCC="Subject: $ID: Connection with $1 is back up"
+BODY_FAIL="You will be informed when it is back up via separate e-mail."
+BODY_SUCC="Remote machine answered the ping."
+
 notification() {
     CURTIME="$(date --iso-8601=seconds)"
     MAILDIR="/tmp/mail/$CURTIME/"
     mkdir -p $MAILDIR
     MAIL="$MAILDIR/notification_message.txt"
     
-    echo "Subject: $ID: Uptime check failed with $1" > $MAIL
+    echo $SUBJECT_FAIL > $MAIL
 
-    echo "You will be informed when it is back up via separate e-mail." >> $MAIL
+    echo $BODY_FAIL >> $MAIL
     echo "---" >> $MAIL
     echo "$CURTIME" >> $MAIL
     
